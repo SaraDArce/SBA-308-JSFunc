89ce8d7d69ddae8e8adf65e509d931efd15715bb
Sara D'Arce <saradarce@gmail.com>
2023-12-06 09:49:24 -0500

    Data set

--- script.js
+++ script.js
@@ -17,7 +17,18 @@ const AssignmentGroup = {
       due_at: "2023-01-25",
       points_possible: 50,
     },
-    // ... (other assignments)
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
   ],
 };
 
@@ -31,7 +42,38 @@ const LearnerSubmissions = [
       score: 47,
     },
   },
-  // ... (other learner submissions)
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
 ];
 
 // Function to check if a submission is late
